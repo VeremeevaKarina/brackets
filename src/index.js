192c443beef70888c51d83835cccdc1d8c452520
karina.v.99933@gmail.com <karina.v.99933@gmail.com>
2021-03-01 21:30:53 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const config = new Map(bracketsConfig);
+  const string = str.split('');
+  const result = string.reduce((array, item) => {
+    if (array.length === 0) {
+      return config.has(item) ? [...array, item] : [...array, item];
+    }
+    else {
+      if (config.get(array[array.length - 1]) === item) {
+        array.pop();
+        return [...array];
+      }
+      else {
+        return [...array, item];
+      }
+    }
+  }, [])
+
+  return result.length === 0;
 }
